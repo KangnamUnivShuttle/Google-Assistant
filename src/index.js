ceb2110fdae8a7db01318e4e04a55ffbd1529498
Kim HyunWoo <stories2@naver.com>
2021-11-11 01:36:28 +0900

    asdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {actionssdk} = require('actions-on-google');
+const googleApp = actionssdk({debug: true});
+
+// https://github.com/actions-on-google/actionssdk-say-number-nodejs/blob/master/functions/index.js
+googleApp.intent('actions.intent.MAIN', (conv) => {
+    conv.ask('<speak>Hi! <break time="1"/> I can read out an ordinal like ' +
+        '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>');
+});
+  
+googleApp.intent('raw.input', (conv, input) => {
+    const number = conv.arguments.get('ordinal');
+    conv.ask(`<speak>You said, <say-as interpret-as="ordinal">${number}</say-as></speak>`);
+});
+  
+  //Handles in-dialog conversation
+  googleApp.intent('actions.intent.TEXT', (conv, input) => {
+    if (input === 'bye') {
+        return conv.close('Goodbye!');
+    }
+    conv.ask(`<speak>You said, <say-as interpret-as="ordinal">${input}</say-as></speak>`);
+});
 
 app.use(express.json())
 
@@ -13,6 +34,9 @@ app.post('/post', (req, res) => {
     console.log('post', req.body)
     res.send('Hello World!')
 })
+
+// https://github.com/actions-on-google/actions-on-google-nodejs
+app.post('/fulfillment', googleApp)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
